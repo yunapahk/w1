f8334608ade480bc672cdb73a751e19ccc3e2cfb
Yuna Pahk <yunapahk@gmail.com>
2023-05-02 11:36:44 -0700

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,24 @@
+// 1. Create a loop that logs the numbers from 0-99 (ascending)
+for (let i = 0; i < 100; i++) {
+    console.log(i);
+  }
+
+// 2. Create a loop that logs the numbers from 99-0 (descending)
+for (let i = 99; i >= 0; i--) {
+    console.log(i);
+  }
+
+// 3. Create a loop that logs the EVEN numbers from 0-98 (ascending)
+for (let i = 0; i < 100; i += 2) {
+    console.log(i);
+  }
+
+// 4. Create a loop that logs the EVEN numbers from 98-0 (descending)
+for (let i = 98; i >= 0; i -= 2) {
+    console.log(i);
+  }
+
+// 5. Create a loop that logs the ODD numbers from 0-99 (ascending)
+for (let i = 1; i < 100; i += 2) {
+  console.log(i);
+}
